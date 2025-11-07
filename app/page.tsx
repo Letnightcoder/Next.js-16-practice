2e2bb0695d3acc3a779e7a8a612c125d972a7ecd
Official-Kallol <official.kallol2024@gmail.com>
2025-11-07 18:00:29 +0530

    routing practice

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
+  
   return (
-    <div className='text-5xl font-bold underline'>Welcome to Next.js</div>
-  )
-}
+    <>
+      <div className="text-5xl font-bold underline">Welcome to Next.js</div>
 
-export default Home
+    </>
+  );
+};
+
+export default Home;
